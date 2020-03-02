31df588dc288ba6c6a32e675f76a6efdf58313da
sabatino arthur <sabatinoarthur2gmail.com>
2020-03-02 09:35:00 -0300

    first commit

---
+++ aula03.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title> Aula 03 </title>
+</head>
+<body>
+
+  <style media="screen">
+  /*.topo{
+    background-color: #b3ffcc;
+  }*/
+  .topo h2{
+    text-align: center;
+  }
+  /*
+  .fundo{
+    background-color:#e6ffee;
+  }*/
+  /*
+  .noticias h1{
+    color:  #00e6e6;
+  }
+*/
+  .noticias p{
+    font-size: 15px;
+    text-align: justify;
+  }
+
+  .noticias img{
+    width: 500px;
+    /* height: ;  */
+  }
+
+  </style>
+
+  <div class="topo">
+    <hr>
+    <h2> Portal de Noticias IFPR </h2>
+    <hr>
+  </div>
+
+
+  <div class="noticias fundo">
+    <h1>Sem anistia, policiais militares votam por encerrar motim no Ceará depois de 13 dias</h1>
+    <img src="https://s2.glbimg.com/4O7z-vLeNnEV8ueoYyy5w1iZnP0=/0x0:751x476/1600x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/B/Z/j9zgzpQyONplsHWLshZg/policiais-decidem-fim-da-greve-ceara.jpg">
+    <p> Os policiais militares que seguiam amotinados no 18º Batalhão da PM,
+      em Fortaleza, votaram por terminar o motim na noite deste domingo (1º).
+      Os policiais aceitaram a proposta definida no mesmo dia pela comissão especial
+      formada por membros dos três poderes no Ceará, assim como por representantes dos policiais.</p>
+      <p> Um dos pontos do acordo é que os policiais retornem aos postos de trabalho nesta segunda-feira (2).</p>
+      <p><ul>
+        <li>Os policiais terão apoio de instituições que não pertencem ao Governo do Estado, como Ordem dos Advogados do Brasil (OAB), Defensoria Pública, Ministério Público e Exército; </li>
+        <li>Os policiais terão direito a um processo legal sem perseguição, com amplo direito a defesa e contraditório, e acompanhamento das instituições mencionadas anteriormente; <br>
+        </li>
+        <li>O Governo do Ceará não vai realizar transferências de policiais para trabalhar no interior do estado em um prazo de 60 dias contados a partir do fim do motim; <br>
+        </li>
+        <li>Revisão de todos os processos adotados contra policiais militares durante a paralisação (entenda os processos abaixo);
+        </li>
+        <li>Garantia de investimento de R$ 495 milhões com o salário de policiais até 2022;
+        </li>
+        <li>Desocupação de todos os batalhões onde havia policiais amotinados até 23h59 deste domingo;
+        </li>
+        <li>Retorno aos postos de trabalho às 8h de segunda-feira. </li></ul></p>
+
+
+        <hr>
+        <div class="noticias">
+          <h1> IBGE divulga as estimativas da população dos municípios para 2019 </h1>
+
+          <p> O IBGE divulga hoje as estimativas das populações residentes nos 5.570 municípios brasileiros, com data de referência em 1º de julho de 2019. Estima-se que o Brasil tenha 210,1 milhões de habitantes e uma taxa de crescimento populacional de 0,79% ao ano, apresentando queda do crescimento quando comparado ao período 2017/2018, conforme a Projeção da População 2018. </p>
+          <p> O município de São Paulo continua sendo o mais populoso do país, com 12,25 milhões de habitantes, seguido pelo Rio de Janeiro (6,72 milhões de habitantes), Brasília (3,0 milhões) e Salvador (2,9 milhões). Juntos, os 17 municípios brasileiros com população superior a um milhão de pessoas somam 46,1 milhões de habitantes ou 21,9% da população do Brasil.</p>
+          <p> Como demonstra a tabela abaixo: </p>
+
+          <table style="width:100%" border="1px">
+            <tr>
+              <th>ORDEM</th>
+              <th>UF</th>
+              <th>MUNICIPIO</th>
+              <th> POPULAÇÃO 2019 </th>
+            </tr>
+            <tr>
+              <td>1°</td>
+              <td>SP</td>
+              <td>São Paulo</td>
+              <td> </td>
+            </tr>
+            <tr>
+              <td>Eve</td>
+              <td>Jackson</td>
+              <td>94</td>
+            </tr>
+          </table>
+        </div>
+
+        <hr>
+        <div class="noticias">
+          Noticia 3 : uma imagem e dois paragrafos
+        </div>
+
+
+      </body>
+      </html>
