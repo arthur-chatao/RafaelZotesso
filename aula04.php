dd35759ffd394fe3ab090642110989f8db29ef4a
Arthur Sabatino <sabatinoarthur@gmail.com>
2020-03-09 09:24:44 -0300

    aula 04

---
+++ aula04.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> Aula 04 - Css e Html</title>
+    <!-- Toda importação de CSS DEVE SER FEITA NO HEAD. -->
+    <link rel="stylesheet" href="aula04.css">
+    <link href="https://fonts.googleapis.com/css?family=Pacifico|Shadows+Into+Light&display=swap" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="site">
+
+      <div class="topo">
+        <h2> Título: Aula 04 </h2>
+
+
+      </div>
+
+      <div class="conteudo">
+        Corpo de site
+
+        <p>
+          Isso eu quero que fique Justificado
+        </p>
+      </div>
+
+      <div class="rodape">
+        Desenvolvido por Arthur Sabatino
+      </div>
+    </div>
+
+  </body>
+</html>
