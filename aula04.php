b44deacc73c2f4bb4fa1423d1b257bec0d6370cb
arthur <sabatinoarthur@gmail.com>
2020-03-16 09:26:55 -0300

    5

--- aula04.php
+++ aula04.php
@@ -4,8 +4,9 @@
     <meta charset="utf-8">
     <title> Aula 04 - Css e Html</title>
     <!-- Toda importação de CSS DEVE SER FEITA NO HEAD. -->
-    <link rel="stylesheet" href="aula04.css">
     <link href="https://fonts.googleapis.com/css?family=Pacifico|Shadows+Into+Light&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="aula04.css">
+
   </head>
 
   <body>
---
+++ aula5.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> Aula 05 - Menu e Botões</title>
+    <!-- Toda importação de CSS DEVE SER FEITA NO HEAD. -->
+    <link href="https://fonts.googleapis.com/css?family=Pacifico|Shadows+Into+Light&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="aula05.css">
+
+  </head>
+
+  <body>
+
+    <div class="site">
+
+      <div class="topo">
+        <h2> Título: Aula 05 - Menus e Botões</h2>
+        <!-- menu -->
+        <div class="menu">
+          <ul>
+            <li><a href="#"> Pagina Inicial  </a></li>
+            <li><a href="#"> Cadasto </a></li>
+            <li><a href="#"> Contato </a></li>
+            <li><a href="#">  Alterar Senha </a></li>
+            <li><a href="#"> Login </a></li>
+          </ul>
+        </div>
+
+
+      </div>
+
+      <div class="conteudo">
+        <h3> Criação de Botões </h3>
+
+        <!-- <button type="button" name="button"> Botão 1 </button> para formularios -->
+        <br>
+        <br>
+        <a href="#" class="botao botao-azul"> Botão Azul </a>
+        <a href="#" class="botao botao-verde"> Botão Verde </a>
+        <a href="#" class="botao botao-amarelo"> Botão Amarelo </a>
+
+      </div>
+
+      <div class="rodape">
+        Desenvolvido por Arthur Sabatino
+      </div>
+    </div>
+
+  </body>
+</html>
